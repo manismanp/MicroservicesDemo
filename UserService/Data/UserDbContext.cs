6e1f6bcb4eccb9004873c903dcf5a6dada090aba
manismanp <55688358+manismanp@users.noreply.github.com>
2024-09-09 22:19:59 +0530

    Update UserDbContext.cs

--- UserService/Data/UserDbContext.cs
+++ UserService/Data/UserDbContext.cs
@@ -7,17 +7,6 @@ namespace UserService.Data
     {
         public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
         public DbSet<User> Users { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Seed initial data
-            modelBuilder.Entity<User>().HasData(
-                new User { Id = 1, Name = "Alice", Email = "alice@microservice.com" },
-                new User { Id = 2, Name = "Bob", Email = "bob@microservice.com" }
-            );
-        }
     }
 
 }

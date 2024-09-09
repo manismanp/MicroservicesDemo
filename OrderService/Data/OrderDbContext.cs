565b0e2af3045afe9155bf2b0e51576ef951388f
manismanp <55688358+manismanp@users.noreply.github.com>
2024-09-09 22:19:25 +0530

    Update OrderDbContext.cs

--- OrderService/Data/OrderDbContext.cs
+++ OrderService/Data/OrderDbContext.cs
@@ -7,17 +7,6 @@ namespace OrderService.Data
     {
         public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options) { }
         public DbSet<Order> Orders { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Seed initial data
-            modelBuilder.Entity<Order>().HasData(
-                new Order { Product = "ProductA", Quantity = 5, UserId = 1 },
-                new Order { Product = "ProductB", Quantity = 3, UserId = 2 }
-            );
-        }
     }
 
 }

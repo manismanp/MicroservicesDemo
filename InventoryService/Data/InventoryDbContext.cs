5e05f60e2310af387f94894af6fa4e7a0c9d1bec
manismanp <55688358+manismanp@users.noreply.github.com>
2024-09-09 22:19:04 +0530

    Update InventoryDbContext.cs

--- InventoryService/Data/InventoryDbContext.cs
+++ InventoryService/Data/InventoryDbContext.cs
@@ -7,17 +7,6 @@ namespace InventoryService.Data
     {
         public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options) { }
         public DbSet<InventoryItem> InventoryItems { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Seed initial data
-            modelBuilder.Entity<InventoryItem>().HasData(
-                new InventoryItem { Id = 1, Name = "ProductA", Stock = 100 },
-                new InventoryItem { Id = 2, Name = "ProductB", Stock = 200 }
-            );
-        }
     }
 
 }
